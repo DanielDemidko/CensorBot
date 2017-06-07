1d69fdc2132c81b756a95168550e65d4c4122fbd
Daniel Demidko <DanielDemidko@gmail.com>
2017-06-07 14:14:49 +1000

    Add files via upload

---
+++ App.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+class App
+{
+    private static readonly
+        TelegramBotClient Bot = new TelegramBotClient("");
+    private static readonly String[] Filter = File.ReadAllLines("Filter.txt");
+    private static readonly Char[] Spliters = "#@\"\\/',.?!:;*&/+~- \n".ToCharArray();
+
+    private static async void BotOnMessage(Object sender, MessageEventArgs e)
+    {
+        if (e.Message.Chat.Type == ChatType.Private || e.Message.Type != MessageType.TextMessage)
+        {
+            return;
+        }
+        if (e.Message.Text.ToLower().Split(Spliters).Any(i => Filter.Contains(i)))
+        {
+            try
+            {
+                await Bot.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
+            }
+            catch (Telegram.Bot.Exceptions.ApiRequestException error)
+            {
+
+            }
+        }
+    }
+
+    private static void Main()
+    {
+        Bot.OnMessage += BotOnMessage;
+        Bot.StartReceiving();
+        Console.ReadKey();
+    }
+}
